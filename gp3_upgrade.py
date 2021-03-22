ccacecdfc5d7acc53ac60dc831a9551df916bb94
sanjdhar <sanjaydhar@hotmail.com>
2021-03-22 17:50:38 -0400

    Update gp3_upgrade.py

--- gp3_upgrade.py
+++ gp3_upgrade.py
@@ -1,30 +1,32 @@
 import boto3
 import json
 import decimal
+import os
 from botocore.exceptions import ClientError
 from datetime import datetime
 
 def lambda_handler(event, context):
-    
+
     # Create a DynamoDB table with a simple primary key "volume_id" and input the name in the below variable
-    DDB_TABLE_NAME = "<Your DynamoDB Table Name Goes Here"
+    #DDB_TABLE_NAME = "gp3_upgrade"
+    #SNS_ARN="arn:aws:sns:us-east-1:065399810791:gp3_upgrade"
     
-    # Add your SNS ARN below with the proper AWS account number
-    SNS_ARN="arn:aws:sns:us-east-1:1234567890:<your SNS topic name>"
-
+    DDB_TABLE_NAME = os.environ['DDB_TABLE_NAME']
+    SNS_ARN=os.environ['SNS_ARN']
+    
     dynamodb = boto3.resource('dynamodb')
-
+    
     table = dynamodb.Table(DDB_TABLE_NAME)
-
+    
     sts_client = boto3.client('sts')
     account_id = sts_client.get_caller_identity()['Account']
-
+    
     ec2_client = boto3.client('ec2')
     ec2 = boto3.resource('ec2')
-
+    
     sns_client = boto3.client('sns')
-
-
+    
+    
     # Helper class to convert a DynamoDB item to JSON.
     class DecimalEncoder(json.JSONEncoder):
         def default(self, o):
@@ -34,25 +36,25 @@ def lambda_handler(event, context):
                 else:
                     return int(o)
             return super(DecimalEncoder, self).default(o)
-
+    
     def check_tags(vol_tags, vol_id):
         upgrade_tag = True
         if not vol_tags is None:
             for tag in vol_tags:
-
+                
                 tag_key=tag['Key'].lower()
                 tag_val=tag.get('Value').lower()
-
+                
                 if tag_key == 'upgrade_to_gp3' and (tag_val == 'no' or tag_val == 'false'):
                     print("No gp3 upgrade set on: ", vol_id)
                     upgrade_tag = False
             return(upgrade_tag)
-
+                 
         else:
             return(upgrade_tag)
-
-
-
+    
+    
+    
     volume_iterator = ec2.volumes.filter(
         Filters=[
             {
@@ -62,24 +64,24 @@ def lambda_handler(event, context):
                 ]
             },
         ],
-
+        
     )
-
+    
     #Array to hold volume list
     vol_list=[]
-
+    
     for v in volume_iterator:
         if(check_tags(v.tags, v.id)):
             vol_list.append(v.id)
-
+    
     response = table.get_item(Key={'volume_id': 'vol-meta-' + account_id})
-
+    
     if('Item') in response:
         run_cnt = response['Item']['run_seq']
         response = table.update_item(
             Key={
                 'volume_id': 'vol-meta-' + account_id,
-
+    
             },
             UpdateExpression="set run_seq = run_seq + :val",
             ExpressionAttributeValues={
@@ -87,19 +89,18 @@ def lambda_handler(event, context):
             },
             ReturnValues="UPDATED_NEW"
         )
-
-
-
+           
+        
+    
         response = table.get_item(
             Key={
                 'volume_id': 'vol-meta-' + account_id,
             }
-
+            
         )
         vol_list=response['Item']['vol_list']
-
+    
         for vol in vol_list:
-            try:
                 volume = ec2.Volume(vol)
                 if volume.volume_type == 'gp2':
                     try:
@@ -113,7 +114,7 @@ def lambda_handler(event, context):
                     response = table.update_item(
                         Key={
                             'volume_id': vol,
-
+    
                         },
                         ExpressionAttributeNames={"#aft": "after_type"},
                         UpdateExpression="set #aft = :aft",
@@ -122,46 +123,40 @@ def lambda_handler(event, context):
                         },
                         ReturnValues="UPDATED_NEW"
                     )
-
+    
                 if (run_cnt > 1):
                     message = 'gp3 volume upgrade status as of ' + datetime.utcnow().strftime("%b %d %Y %H:%M:%S UTC") + ':\n'
                     message = message + """\n-------------\t\t\t\t-------------\t\t\t\t--------------\n
     Volume ID\t\t\t\tAccount ID\t\t\t\tStatus\n-------------\t\t\t\t-------------\t\t\t\t--------------\n
     """
                     response = table.get_item(
-                    Key={
-                        'volume_id': 'vol-meta-' + account_id,
-                        }
-
-                    )
-            except ClientError as e:
-                print(e.response['Error']['Message'])
-
+                Key={
+                    'volume_id': 'vol-meta-' + account_id,
+                }
+                
+            )
         vol_list=response['Item']['vol_list']
-
-        try:
-            for vol in vol_list:
-                    response = table.get_item(
-                        Key={
-                            'volume_id': vol,
-                        }
-
-                        )
-                    vol_status=response['Item']['after_type']
-                    if (vol_status == 'gp3'):
-                            message = message + vol + '\t\t' + account_id + '\t\t\t\t' +'Successful\n'
-                    else:
-                            message = message + vol + '\t\t' + account_id + '\t\t\t\t' + 'Failed. Please review CloudWatch Logs for details\n'
-
-            message = message + '==============================End of Message ======================================\n'
-
-            response = sns_client.publish(
-                    TopicArn=SNS_ARN,   
-                    Subject="gp3 upgrade status",
-                    Message=message,   
-                )
-        except ClientError as e:
-                print(e.response['Error']['Message'])
+    
+        for vol in vol_list:
+                response = table.get_item(
+                    Key={
+                        'volume_id': vol,
+                    }
+                
+                    )
+                vol_status=response['Item']['after_type']
+                if (vol_status == 'gp3'):
+                        message = message + vol + '\t\t' + account_id + '\t\t\t\t' +'Successful\n'
+                else:
+                        message = message + vol + '\t\t' + account_id + '\t\t\t\t' + 'Failed. Please check CloudWatch Logs for details\n'
+                
+        message = message + '==============================End of Message ======================================\n'
+    
+        response = sns_client.publish(
+                TopicArn=SNS_ARN,   
+                Subject="gp3 upgrade status",
+                Message=message,   
+            )
     else:
         response = table.put_item(
             Item={
@@ -170,10 +165,10 @@ def lambda_handler(event, context):
                     'run_seq': 1,
                 }
         ) 
-
+    
         try:
             for v in volume_iterator:
-
+                                     
                 if (check_tags(v.tags, v.id)): 
                     response = table.put_item(
                     Item={
@@ -184,18 +179,19 @@ def lambda_handler(event, context):
                         'size': v.size,
                         'tags': v.tags,
                         'Account ID': account_id,
-
+                    
                         }
                     )
-
+    
                     try:
                         vol_mod = ec2_client.modify_volume(
                             VolumeId = v.id,
                             VolumeType = 'gp3'
-
+    
                         )
                     except ClientError as e:
                         print(e.response['Error']['Message'])
-
+    
         except ClientError as e:
             print(e.response['Error']['Message'])
+
